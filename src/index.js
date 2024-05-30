7adfb827b96bc32e4abd0fde45740952ec9390d7
Daniel <dandcalvo@gmail.com>
2024-05-30 16:42:30 -0700

    populate map

--- src/index.js
+++ src/index.js
@@ -2,13 +2,6 @@ import leaf from 'leaflet';
 import './style/style.scss';
 import '../node_modules/leaflet/dist/leaflet.css';
 
-/* const map = leaf.map('map').setView([37.656, -122.093], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map); */
-
 function loadFiles() {
     const context = require.context('./datafiles/', true, /\.geojson$/);
     const cache = {};
@@ -20,4 +13,19 @@ function loadFiles() {
     return cache;
 }
 
-console.log(loadFiles());
+function displayGeojsonFeatures(features) {
+    Object.keys(features).forEach((key) => {
+        leaf.geoJSON(features[key]).addTo(map);
+    });
+}
+
+/* Load Map */
+const map = leaf.map('map').setView([37.6688, -122.081], 13);
+leaf.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+const geojsonFeatures = loadFiles();
+displayGeojsonFeatures(geojsonFeatures);
