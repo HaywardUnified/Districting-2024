732eaf24639aebc83bd8a9cfd93877a72260ce9b
Daniel <dandcalvo@gmail.com>
2024-05-31 12:27:31 -0700

    geoLayer generator

--- src/index.js
+++ src/index.js
@@ -2,30 +2,46 @@ import leaf from 'leaflet';
 import './style/style.scss';
 import '../node_modules/leaflet/dist/leaflet.css';
 
+const STARTING_COORDINATES = [37.6688, -122.081];
+
 function loadFiles() {
     const context = require.context('./datafiles/', true, /\.geojson$/);
-    const cache = {};
+    const files = {};
 
     context.keys().forEach((key) => {
-        cache[key] = context(key);
+        const formattedKey = key
+            .replace('./', '')
+            .replace(/\.(json|geojson)$/, '');
+
+        files[formattedKey] = context(key);
     });
 
-    return cache;
+    return files;
 }
 
-function displayGeojsonFeatures(features) {
+/**
+ * Creates geo layers for each feature
+ * @param {object} features - GeoJSON features
+ * @returns map containing layer instances
+ */
+function generateGeoLayers(features) {
+    const layers = new Map();
+
     Object.keys(features).forEach((key) => {
-        leaf.geoJSON(features[key]).addTo(map);
+        layers.set(key, leaf.geoJSON(features[key]));
     });
+
+    return layers;
 }
 
 /* Load Map */
-const map = leaf.map('map').setView([37.6688, -122.081], 13);
+const map = leaf.map('map').setView(STARTING_COORDINATES, 13);
 leaf.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution:
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-const geojsonFeatures = loadFiles();
-displayGeojsonFeatures(geojsonFeatures);
+const geoFeatures = loadFiles();
+const geoLayers = generateGeoLayers(geoFeatures);
+console.log(geoFeatures);
