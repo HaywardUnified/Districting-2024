54644ff241625989dcc29e94989bf8b8e05f5eca
Daniel <dandcalvo@gmail.com>
2024-06-03 12:58:25 -0700

    base layers and overlays

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import leaf from 'leaflet';
+import leaf, { layerGroup, polygon } from 'leaflet';
 import './style/style.scss';
 import '../node_modules/leaflet/dist/leaflet.css';
 
@@ -9,6 +9,44 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 =======
 =======
 const STARTING_COORDINATES = [37.6688, -122.081];
+const osm = leaf.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+        '&copy; <a target="_blank" href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    maxZoom: 19,
+});
+const googleHybrid = leaf.tileLayer(
+    'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
+    {
+        attribution:
+            '&copy; <a target="_blank" href="https://policies.google.com/">Google</a>',
+        maxZoom: 19,
+    }
+);
+const googleSatellite = leaf.tileLayer(
+    'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+    {
+        attribution:
+            '&copy; <a target="_blank" href="https://policies.google.com/">Google</a>',
+        maxZoom: 19,
+    }
+);
+
+const baseLayers = {
+    Digital: osm,
+    Hybrid: googleHybrid,
+    Satellite: googleSatellite,
+};
+const geoFeatureCollections = loadFiles(); // All available geoJSON files
+const mapOverlayLayers = {};
+const overlays = {};
+
+//
+geoFeatureCollections.forEach((collection) => {
+    // convert feature collections to geoJSON objects
+    const geoJSONobj = applyFeatureOptions(collection.features);
+    const layer = leaf.layerGroup([geoJSONobj]);
+    mapOverlayLayers[`${collection.name}`] = layer;
+});
 
 <<<<<<< HEAD
 >>>>>>> 732eaf2 (geoLayer generator)
@@ -45,6 +83,7 @@ function generateGeoLayers(features) {
 =======
 >>>>>>> 8da21c5 (tool tip and colors)
 /* Load Map */
+<<<<<<< HEAD
 const map = leaf.map('map').setView(STARTING_COORDINATES, 12);
 const geoFeatureCollections = loadFiles();
 const mapLayerGroup = leaf.layerGroup(geoFeatureCollections);
@@ -79,17 +118,35 @@ geoFeatureCollections.forEach((collection) => {
     applyFeatureOptions(collection.features).addTo(map);
 
     console.log(collection);
+=======
+const map = leaf.map('map', {
+    center: STARTING_COORDINATES,
+    layers: [googleHybrid],
+    zoom: 12,
+>>>>>>> a02c6f9 (base layers and overlays)
 });
 
-mapLayerGroup.getLayers().forEach((layer) => {
-    layer.features.forEach((feature) => {
-        leaf.geoJSON(feature);
-    });
+const baseOverlayControls = createControls(baseLayers, overlays);
+const mapOverlayControls = createControls(mapOverlayLayers, null, {
+    collapsed: false,
 });
+
+console.log(geoFeatureCollections);
+console.log(overlays);
+
+/**
+ * Create a layer control to store base layers and overlays.
+ * @param {*} baseLayers - tile layers (ie. maps)
+ * @param {*} overlays - stuff that goes over base layers (ie. markers, features)
+ * @return {object} layerControl
+ */
+function createControls(baseLayers, overlays, options) {
+    return leaf.control.layers(baseLayers, overlays, options).addTo(map);
+}
 
 /**
  * Load and convert .geojson files to Geo Feature Collections
- * @returns - geo feature collection
+ * @returns - object containing geo feature collection
  */
 function loadFiles() {
     const context = require.context('./datafiles/', true, /\.geojson$/);
@@ -117,29 +174,34 @@ function applyFeatureOptions(features) {
         onEachFeature: (feature, layer) => {
             layer.bindTooltip(feature.properties.DistrictName);
         },
-        style: function (feature) {
-            let color;
-
-            switch (feature.properties.DistrictName) {
-                case 'A':
-                    color = '#ffbe0b';
-                    break;
-                case 'B':
-                    color = '#fb5607';
-                    break;
-                case 'C':
-                    color = '#ff006e';
-                    break;
-                case 'D':
-                    color = '#8338ec';
-                    break;
-                case 'E':
-                    color = '#3a86ff';
-                    break;
-            }
-
-            return { color };
-        },
+        style: styleFeature,
     });
 }
+<<<<<<< HEAD
 >>>>>>> 8da21c5 (tool tip and colors)
+=======
+
+function styleFeature(feature) {
+    let color;
+
+    switch (feature.properties.DistrictName) {
+        case 'A':
+            color = '#ffbe0b';
+            break;
+        case 'B':
+            color = '#fb5607';
+            break;
+        case 'C':
+            color = '#ff006e';
+            break;
+        case 'D':
+            color = '#8338ec';
+            break;
+        case 'E':
+            color = '#3a86ff';
+            break;
+    }
+
+    return { color };
+}
+>>>>>>> a02c6f9 (base layers and overlays)
